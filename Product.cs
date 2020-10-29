fc8891357a2794258e8adc9ff9b2e61caa90ee10
tolache <anatoly.cherenkov@gmail.com>
2020-10-29 20:36:47 +0300

    Add sample code

---
+++ Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nuget_consuming_test
+{
+    public class Product
+    {
+        public string Name => "Apple";
+        public DateTime ExpiryDate => new DateTime(2008, 12, 28);
+        public decimal Price => 3.99M;
+        public string[] Sizes => new string[] { "Small", "Medium", "Large" };
+    }
+}
---
+++ ProductSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace nuget_consuming_test
+{
+    public class ProductSerializer
+    {
+        private Product _product = new Product();
+        public string SerializedProduct
+        {
+            get
+            {
+                string output = JsonConvert.SerializeObject(_product);;
+                Console.WriteLine(output);
+                return output;
+            }
+        }
+    }
+}
