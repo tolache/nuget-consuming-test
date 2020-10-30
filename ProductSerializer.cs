93075579c03f4a658a6d502009843e4f7848def2
tolache <anatoly.cherenkov@gmail.com>
2020-10-30 09:41:21 +0300

    remove Newtonsoft.Json dependency

--- ProductSerializer.cs
+++ ProductSerializer.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using Newtonsoft.Json;
 
 namespace nuget_consuming_test
 {
     public class ProductSerializer
     {
         private Product _product = new Product();
-        public string SerializedProduct
+        public string GetProductName
         {
             get
             {
-                string output = JsonConvert.SerializeObject(_product);;
-                Console.WriteLine(output);
-                return output;
+                Console.WriteLine("Returning the product name.");
+                return "TBD";
             }
         }
     }
