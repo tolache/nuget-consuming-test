4b281d8fd1a6971512f235f7001d24dd24bc4ea8
tolache <anatoly.cherenkov@gmail.com>
2020-10-30 10:12:03 +0300

    Implement ProducePackage.sln

---
+++ ProductNameGetter/ProductNameGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProducePackage
+{
+    public class ProductNameGetter
+    {
+        public string GetProductName(object product)
+        {
+            Type type = product.GetType();
+            if (type.GetProperty("Name") != null)
+            {
+                return product.GetType().GetProperty("Name")?.GetValue((dynamic)product);
+            }
+
+            throw new PropertyNotFoundException("Object doesn't have a 'Name' property.");
+        }
+    }
+}
